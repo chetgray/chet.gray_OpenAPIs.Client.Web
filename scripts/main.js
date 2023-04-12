fafb1063f91a264a8db0f5ea18af34d5e155dd95
Chet Gray <chet.gray@waystar.com>
2023-04-12 16:06:03 -0400

    ♻️ refactor: extract function `handleXhrLoad`

--- scripts/main.js
+++ scripts/main.js
@@ -33,20 +33,14 @@ $(function () {
     $btnPlace.on("click", function () {
         const country = /** @type {string} */ ($placeCodeCountry.val());
         const postCode = /** @type {string} */ ($placePostCode.val());
+        const $results = $placeResults;
 
         const url = new URL(`https://www.zippopotam.us/${country}/${postCode}`);
         const xhr = new XMLHttpRequest();
-        xhr.addEventListener("load", function () {
-            if (this.status === 200) {
-                const data = JSON.parse(this.responseText);
-                $placeResults.text(JSON.stringify(data, null, 2));
-            } else {
-                $placeResults.text("Error: " + this.status);
-            }
-        });
+        xhr.addEventListener("load", handleXhrLoad.bind(xhr, $results));
 
         xhr.open("GET", url);
-        $placeResults.text("Loading...");
+        $results.text("Loading...");
         xhr.send();
 
         return false;
@@ -56,20 +50,14 @@ $(function () {
         const country = /** @type {string} */ ($postCodeCountry.val());
         const state = /** @type {string} */ ($postCodeState.val());
         const placeName = /** @type {string} */ ($postCodePlaceName.val());
+        const $results = $postCodeResults;
 
         const url = new URL(`https://api.zippopotam.us/${country}/${state}/${placeName}`);
         const xhr = new XMLHttpRequest();
-        xhr.addEventListener("load", function () {
-            if (this.status === 200) {
-                const data = JSON.parse(this.responseText);
-                $postCodeResults.text(JSON.stringify(data, null, 2));
-            } else {
-                $postCodeResults.text("Error: " + this.status);
-            }
-        });
+        xhr.addEventListener("load", handleXhrLoad.bind(xhr, $results));
 
         xhr.open("GET", url);
-        $postCodeResults.text("Loading...");
+        $results.text("Loading...");
         xhr.send();
 
         return false;
@@ -80,22 +68,16 @@ $(function () {
         const longitude = /** @type {string} */ ($sunriseSunsetLongitude.val());
         const date = /** @type {string} */ ($sunriseSunsetDate.val());
         const timeZone = /** @type {string} */ ($sunriseSunsetTimeZone.val());
+        const $results = $sunriseSunsetResults;
 
         const url = new URL(
             `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&date=${date}&date=${date}&time=${timeZone}`
         );
         const xhr = new XMLHttpRequest();
-        xhr.addEventListener("load", function () {
-            if (this.status === 200) {
-                const data = JSON.parse(this.responseText);
-                $sunriseSunsetResults.text(JSON.stringify(data, null, 2));
-            } else {
-                $sunriseSunsetResults.text("Error: " + this.status);
-            }
-        });
+        xhr.addEventListener("load", handleXhrLoad.bind(xhr, $results));
 
         xhr.open("GET", url);
-        $sunriseSunsetResults.text("Loading...");
+        $results.text("Loading...");
         xhr.send();
 
         return false;
@@ -104,22 +86,16 @@ $(function () {
     $btnWeatherForecast.on("click", function () {
         const latitude = /** @type {string} */ ($weatherForecastLatitude.val());
         const longitude = /** @type {string} */ ($weatherForecastLongitude.val());
+        const $results = $weatherForecastResults;
 
         const url = new URL(
             `https://www.7timer.info/bin/api.pl?lon=${longitude}&lat=${latitude}&product=astro&output=json`
         );
         const xhr = new XMLHttpRequest();
-        xhr.addEventListener("load", function () {
-            if (this.status === 200) {
-                const data = JSON.parse(this.responseText);
-                $weatherForecastResults.text(JSON.stringify(data, null, 2));
-            } else {
-                $weatherForecastResults.text("Error: " + this.status);
-            }
-        });
+        xhr.addEventListener("load", handleXhrLoad.bind(xhr, $results));
 
         xhr.open("GET", url);
-        $weatherForecastResults.text("Loading...");
+        $results.text("Loading...");
         xhr.send();
 
         return false;
@@ -129,24 +105,42 @@ $(function () {
         const latitude = /** @type {string} */ ($breweryLatitude.val());
         const longitude = /** @type {string} */ ($breweryLongitude.val());
         const type = /** @type {string} */ ($breweryType.val());
+        const $results = $breweryResults;
 
         const url = new URL(
             `https://api.openbrewerydb.org/breweries?by_dist=${latitude},${longitude}&per_page=3&by_type=${type}`
         );
         const xhr = new XMLHttpRequest();
-        xhr.addEventListener("load", function () {
-            if (this.status === 200) {
-                const data = JSON.parse(this.responseText);
-                $breweryResults.text(JSON.stringify(data, null, 2));
-            } else {
-                $breweryResults.text("Error: " + this.status);
-            }
-        });
+        xhr.addEventListener("load", handleXhrLoad.bind(xhr, $results));
 
         xhr.open("GET", url);
-        $breweryResults.text("Loading...");
+        $results.text("Loading...");
         xhr.send();
 
         return false;
     });
 });
+
+/**
+ * @param {JQuery} $results
+ * @param {ProgressEvent<XMLHttpRequest>} event
+ */
+function handleXhrLoad($results, event) {
+    const xhr = event.target;
+    if (xhr.status !== 200) {
+        const message = `Error: ${xhr.status}`;
+        console.error(message);
+        $results.text(message);
+        return;
+    }
+    try {
+        var data = JSON.parse(xhr.responseText);
+    } catch (error) {
+        const message = `Error: ${error.message}`;
+        console.error(message);
+        $results.text(message);
+        return;
+    }
+    console.log(data);
+    $results.text(JSON.stringify(data, null, 2));
+}
